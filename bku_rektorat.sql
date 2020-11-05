515bcf38b9d4a5df0209317bc53739f7895fc3be
Yusupxyz <yusufxyz114@gmail.com>
2020-11-05 07:11:17 +0700

    db baru

--- bku_rektorat.sql
+++ bku_rektorat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Mar 21, 2020 at 02:27 PM
+-- Generation Time: Nov 05, 2020 at 01:10 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -68,22 +68,20 @@ INSERT INTO `groups_menu` (`id_groups`, `id_menu`) VALUES
 (1, 3),
 (2, 3),
 (3, 3),
-(1, 40),
 (1, 9),
 (2, 9),
 (3, 9),
 (1, 93),
 (1, 94),
 (1, 95),
-(1, 96),
-(1, 97),
-(1, 98),
 (1, 91),
 (1, 1),
 (1, 99),
 (1, 100),
 (1, 101),
-(1, 102);
+(1, 102),
+(1, 103),
+(0, 40);
 
 -- --------------------------------------------------------
 
@@ -97,6 +95,15 @@ CREATE TABLE `login_attempts` (
   `login` varchar(100) NOT NULL,
   `time` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+INSERT INTO `login_attempts` (`id`, `ip_address`, `login`, `time`) VALUES
+(1, '::1', 'admin', 1604479292),
+(2, '::1', 'admin', 1604479300),
+(3, '::1', 'admin', 1604479304);
 
 -- --------------------------------------------------------
 
@@ -123,25 +130,23 @@ CREATE TABLE `menu` (
 INSERT INTO `menu` (`id_menu`, `sort`, `level`, `parent_id`, `icon`, `label`, `link`, `id`, `id_menu_type`) VALUES
 (1, 0, 1, 0, 'empty', 'Utama', '#', '#', 1),
 (3, 1, 2, 1, 'fas fa-tachometer-alt', 'Dashboard', 'dashboard', '#', 1),
-(4, 14, 2, 40, 'fas fa-table', 'CRUD Generator', 'crudbuilder', '1', 1),
-(8, 12, 2, 40, 'fas fa-bars', 'Menu', 'cms/menu/side-menu', 'navMenu', 1),
-(40, 11, 1, 0, 'empty', 'SETTING', '#', '#', 1),
-(42, 15, 2, 40, 'fas fa-users-cog', 'User', '#', '1', 1),
-(43, 16, 3, 42, 'fas fa-angle-double-right', 'Users', 'users', '1', 1),
-(44, 17, 3, 42, 'fas fa-angle-double-right', 'Groups', 'groups', '2', 1),
-(89, 13, 2, 40, 'fas fa-th-list', 'Menu Type', 'menu_type', 'menu_type', 1),
-(91, 8, 2, 92, 'far fa-calendar-times', 'Bulan', 'bulan', '12', 1),
-(92, 3, 1, 0, 'empty', 'DATA MASTER', '#', '#', 1),
-(93, 9, 2, 92, 'fas fa-calendar-times', 'Tahun', 'tahun', '13', 1),
-(94, 4, 2, 92, 'fab fa-monero', 'Jenis Pembayaran', 'jenis_pembayaran', '14', 1),
-(95, 5, 2, 92, 'fab fa-asymmetrik', 'Metode Pembayaran', 'metode_pembayaran', '15', 1),
-(96, 10, 2, 92, 'fab fa-mizuni', 'Unit', 'unit', '15', 1),
-(97, 6, 2, 92, 'fas fa-money-bill-wave', 'Saldo Awal', 'saldo_awal', '16', 1),
-(98, 7, 2, 92, 'fas fa-money-bill-wave', 'Saldo Akhir', 'saldo_akhir', '17', 1),
-(99, 1, 2, 1, 'fas fa-file-contract', 'Kontrol Transaksi', 'transaksi', '#', 1),
-(100, 1, 2, 92, 'fas fa-book-reader', 'Pejabat', 'pejabat', '13', 1),
-(101, 4, 2, 1, 'fas fa-book-open', 'Buku', 'buku', '#', 1),
-(102, 1, 2, 92, 'fas fa-mouse-pointer', 'Atur Laporan', 'setting_laporan', '#', 1);
+(4, 15, 2, 40, 'fas fa-table', 'CRUD Generator', 'crudbuilder', '1', 1),
+(8, 13, 2, 40, 'fas fa-bars', 'Menu', 'cms/menu/side-menu', 'navMenu', 1),
+(40, 12, 1, 0, 'empty', 'SETTING', '#', '#', 1),
+(42, 16, 2, 40, 'fas fa-users-cog', 'User', '#', '1', 1),
+(43, 17, 3, 42, 'fas fa-angle-double-right', 'Users', 'users', '1', 1),
+(44, 18, 3, 42, 'fas fa-angle-double-right', 'Groups', 'groups', '2', 1),
+(89, 14, 2, 40, 'fas fa-th-list', 'Menu Type', 'menu_type', 'menu_type', 1),
+(91, 10, 2, 92, 'far fa-calendar-times', 'Bulan', 'bulan', '12', 1),
+(92, 5, 1, 0, 'empty', 'DATA MASTER', '#', '#', 1),
+(93, 11, 2, 92, 'fas fa-calendar-times', 'Tahun', 'tahun', '13', 1),
+(94, 8, 2, 92, 'fab fa-monero', 'Jenis Pembayaran', 'jenis_pembayaran', '14', 1),
+(95, 9, 2, 92, 'fab fa-asymmetrik', 'Metode Pembayaran', 'metode_pembayaran', '15', 1),
+(99, 2, 2, 1, 'fas fa-file-contract', 'Kontrol Transaksi', 'transaksi', '#', 1),
+(100, 6, 2, 92, 'fas fa-book-reader', 'Pejabat', 'pejabat', '13', 1),
+(101, 3, 2, 1, 'fas fa-book-open', 'Buku', 'buku', '#', 1),
+(102, 7, 2, 92, 'fas fa-mouse-pointer', 'Atur Laporan', 'setting_laporan', '#', 1),
+(103, 4, 2, 1, 'fas fa-download', 'Unduh Excel', 'unduh', '1', 1);
 
 -- --------------------------------------------------------
 
@@ -177,8 +182,6 @@ CREATE TABLE `tbl_bulan` (
 --
 
 INSERT INTO `tbl_bulan` (`bulan_id`, `bulan_nama`) VALUES
-(1, 'Januari'),
-(2, 'Februari'),
 (3, 'Maret'),
 (4, 'April'),
 (5, 'Mei'),
@@ -207,7 +210,8 @@ CREATE TABLE `tbl_jenis_pembayaran` (
 
 INSERT INTO `tbl_jenis_pembayaran` (`jp_id`, `jp_nama`) VALUES
 (1, 'Bank'),
-(2, 'Tunai');
+(2, 'Tunai'),
+(3, 'Transfer BP');
 
 -- --------------------------------------------------------
 
@@ -227,28 +231,6 @@ CREATE TABLE `tbl_metode_pembayaran` (
 INSERT INTO `tbl_metode_pembayaran` (`mp_id`, `mp_nama`) VALUES
 (1, 'GU'),
 (2, 'LS');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_nomor_bukti`
---
-
-CREATE TABLE `tbl_nomor_bukti` (
-  `nb_id` int(11) NOT NULL,
-  `nb_no` varchar(100) NOT NULL,
-  `nb_tanggal` date NOT NULL,
-  `uraian` text NOT NULL,
-  `tbl_pengeluaran` double NOT NULL,
-  `nb_id_tahun` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_nomor_bukti`
---
-
-INSERT INTO `tbl_nomor_bukti` (`nb_id`, `nb_no`, `nb_tanggal`, `uraian`, `tbl_pengeluaran`, `nb_id_tahun`) VALUES
-(4, '0000/PNBP Non Modal-UPR/2020', '2020-03-19', 'Bayar ke BPP BUK', 0, 10);
 
 -- --------------------------------------------------------
 
@@ -383,9 +365,8 @@ INSERT INTO `tbl_tahun` (`tahun_id`, `tahun_nama`, `tahun_status`) VALUES
 
 CREATE TABLE `tbl_transaksi` (
   `trx_id` int(11) NOT NULL,
-  `trx_id_nomor_bukti` int(11) NOT NULL,
+  `trx_nomor_bukti` varchar(100) NOT NULL,
   `trx_mak` varchar(100) NOT NULL,
-  `trx_penerima` varchar(100) NOT NULL,
   `trx_uraian` varchar(500) NOT NULL,
   `trx_jml_kotor` double NOT NULL,
   `trx_ppn` double NOT NULL,
@@ -397,52 +378,54 @@ CREATE TABLE `tbl_transaksi` (
   `trx_tanggal` date NOT NULL,
   `trx_id_jenis_pembayaran` int(11) NOT NULL,
   `trx_id_metode_pembayaran` int(11) NOT NULL,
-  `trx_id_unit` int(11) NOT NULL
+  `trx_id_unit` varchar(300) NOT NULL,
+  `trx_jenis` enum('0','1') NOT NULL,
+  `trx_penerimaan` double NOT NULL,
+  `trx_pengeluaran` double NOT NULL,
+  `trx_id_tahun` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_transaksi`
 --
 
-INSERT INTO `tbl_transaksi` (`trx_id`, `trx_id_nomor_bukti`, `trx_mak`, `trx_penerima`, `trx_uraian`, `trx_jml_kotor`, `trx_ppn`, `trx_pph_21`, `trx_pph_22`, `trx_pph_23`, `trx_pph_4_2`, `trx_jml_bersih`, `trx_tanggal`, `trx_id_jenis_pembayaran`, `trx_id_metode_pembayaran`, `trx_id_unit`) VALUES
-(4, 4, '5742.994.051.H.521111', '-', 'ATK dan bahan habis pakai, penggandaan, penjilidan, pencetakan, dokumentasi Operasional Rapat Senat Tertutup Universitas Palangka Raya (tgl. 23 Januari 2020)', 2000000, 181818, 0, 0, 0, 0, 1818182, '2020-03-20', 1, 1, 0),
-(5, 4, '5742.994.051.H.521111', '-', 'Konsumsi makan rapat senat  [50 ORG x Rp. 40.000 x 1 KEG]', 2000000, 0, 0, 0, 3636, 0, 1996364, '2020-03-21', 1, 1, 0);
+INSERT INTO `tbl_transaksi` (`trx_id`, `trx_nomor_bukti`, `trx_mak`, `trx_uraian`, `trx_jml_kotor`, `trx_ppn`, `trx_pph_21`, `trx_pph_22`, `trx_pph_23`, `trx_pph_4_2`, `trx_jml_bersih`, `trx_tanggal`, `trx_id_jenis_pembayaran`, `trx_id_metode_pembayaran`, `trx_id_unit`, `trx_jenis`, `trx_penerimaan`, `trx_pengeluaran`, `trx_id_tahun`) VALUES
+(9, '200431304005195', '', 'Penyediaan Uang Persediaan PNBP Satker UNIVERSITAS PALANGKARAYA Tahun 2020 Sesuai SPP Nomor 00224 Tanggal 18 Maret 2020', 0, 0, 1, 0, 0, 0, 0, '2020-07-07', 1, 2, 'PNBP', '0', 500000000, 0, 10),
+(11, '200431304005152', '4257.015.054.A.521115', 'Pembayaran Belanja Barang Sesuai Dengan SK.No.37/UN24/KP/2020. Tgl.10 Februari 2020. Bulan Januari 2020. (Tenaga Kontrak Pramubakti, Petugas Kebersihan, Pengemudi & Satpam Bersumber Dari Anggaran PNBP T.A.2020). Sesuai SPP Nomor 00227 Tangg', 0, 0, 0, 0, 0, 0, 0, '2020-07-06', 1, 2, 'Tenaga Kontrak Pramubakti, Petugas Kebersihan, Pengemudi & Satpam Bersumber Dari Anggaran PNBP T.A.2020', '0', 424291998, 0, 10),
+(13, '200431304005745', '4257.015.054.A.521115', 'Biaya Perjalanan Dinas Dalam Rangka Menghadiri Undangan Rapat Koordinasi Nasional (Rakornas) Kemenristek/ Badan Riset dan Inovasi Nasional Tahun 2020 (Rakornas RISTEK/BRIN 2020), sesuai dengan surat', 6478656, 0, 0, 0, 0, 0, 6478656, '2020-07-22', 2, 2, 'Estiani Isa Leluni, SH', '0', 0, 6478656, 10),
+(14, '200431304005745', '4257.015.054.A.521115', 'Biaya Perjalanan Dinas Dalam Rangka Menghadiri Undangan Rapat Koordinasi Nasional (Rakornas) Kemenristek/ Badan Riset dan Inovasi Nasional Tahun 2020 (Rakornas RISTEK/BRIN 2020), sesuai dengan surat', 6478656, 0, 0, 0, 0, 0, 6478656, '2020-07-22', 2, 2, 'Estiani Isa Leluni, SH', '0', 0, 6478656, 10),
+(15, '200431304005745', '4257.015.054.A.521115', 'Biaya Perjalanan Dinas Dalam Rangka Menghadiri Undangan Rapat Koordinasi Nasional (Rakornas) Kemenristek/ Badan Riset dan Inovasi Nasional Tahun 2020 (Rakornas RISTEK/BRIN 2020), sesuai dengan surat', 6478656, 0, 0, 0, 0, 0, 6478656, '2020-07-22', 2, 2, 'Estiani Isa Leluni, SH', '0', 0, 6478656, 10);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `unit`
+-- Table structure for table `tbl_transaksi_unit`
 --
 
-CREATE TABLE `unit` (
-  `id_unit` int(11) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `deskripsi` text NOT NULL
+CREATE TABLE `tbl_transaksi_unit` (
+  `trxu_id` int(11) NOT NULL,
+  `trxu_nomor_bukti` int(3) NOT NULL,
+  `trxu_mak` varchar(100) NOT NULL,
+  `trxu_uraian` varchar(500) NOT NULL,
+  `trxu_jml_kotor` double NOT NULL,
+  `trxu_ppn` double NOT NULL,
+  `trxu_pph_21` double NOT NULL,
+  `trxu_pph_22` double NOT NULL,
+  `trxu_pph_23` double NOT NULL,
+  `trxu_pph_4_2` double NOT NULL,
+  `trxu_jml_bersih` double NOT NULL,
+  `trxu_tanggal` date NOT NULL,
+  `trxu_id_jenis_pembayaran` int(11) NOT NULL,
+  `trxu_id_metode_pembayaran` int(11) NOT NULL,
+  `trxu_id_unit` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `unit`
+-- Dumping data for table `tbl_transaksi_unit`
 --
 
-INSERT INTO `unit` (`id_unit`, `nama`, `deskripsi`) VALUES
-(0, 'A', 'Rektorat'),
-(15, 'E', 'Fakultas Pertanian'),
-(16, 'D', 'Fakultas Ekonomi'),
-(17, 'H', 'Fakultas Hukum'),
-(18, 'I', 'Fakultas Ilmu Sosial dan Ilmu Politik'),
-(19, 'J', 'Fakultas Kedokteran'),
-(20, 'C', 'Fakultas Keguruan dan Ilmu Pendidikan'),
-(21, 'L', 'Fakultas Matematikan dan Ilmu Pengetahuan Alam'),
-(22, 'F', 'Fakultas Teknik'),
-(23, 'K', 'Program Pasca Sarjana'),
-(24, 'T', 'LAB ALAM'),
-(25, 'V', 'UPT Bahasa'),
-(26, 'S', 'LAB TERPADU'),
-(27, 'Q', 'UPT Laboratorium Lahan Gambut'),
-(28, 'N', 'LP3MP'),
-(29, 'M', 'LPPM'),
-(30, 'Y', 'UPT Perpustakaan'),
-(31, 'R', 'UPT TIK');
+INSERT INTO `tbl_transaksi_unit` (`trxu_id`, `trxu_nomor_bukti`, `trxu_mak`, `trxu_uraian`, `trxu_jml_kotor`, `trxu_ppn`, `trxu_pph_21`, `trxu_pph_22`, `trxu_pph_23`, `trxu_pph_4_2`, `trxu_jml_bersih`, `trxu_tanggal`, `trxu_id_jenis_pembayaran`, `trxu_id_metode_pembayaran`, `trxu_id_unit`) VALUES
+(2, 13, 'xxxx', 'assasa', 1000000, 0, 0, 0, 0, 0, 1000000, '2020-07-23', 2, 2, 'BPP FISIP');
 
 -- --------------------------------------------------------
 
@@ -475,7 +458,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'WcHCQ5vcXwT1z99BvJUWnu', 1268889823, 1584790988, 1, 'Admin', 'istrator', 'ADMIN', '0'),
+(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'WcHCQ5vcXwT1z99BvJUWnu', 1268889823, 1604534953, 1, 'Admin', 'istrator', 'ADMIN', '0'),
 (2, '127.0.0.1', 'member', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'member@member.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'lHtbqmxsnla1izZ5LcXd9O', 1268889823, 1561843839, 1, 'Operator', 'Apps', 'Prodi', '0'),
 (3, '127.0.0.1', 'adeade', '$2y$08$qbEiq9GhlaB8My8TdJXLPuXKxleROxxQGsCArdMBPfa4gabaja3Xm', 'adeade', 'ade.chandra.saputra.tumbai@gmail.com', NULL, 'ads', 0, 'ad', 0, 1561844915, 1, 'ad', 'e', 'adecs', '12424');
 
@@ -547,12 +530,6 @@ ALTER TABLE `tbl_metode_pembayaran`
   ADD PRIMARY KEY (`mp_id`);
 
 --
--- Indexes for table `tbl_nomor_bukti`
---
-ALTER TABLE `tbl_nomor_bukti`
-  ADD PRIMARY KEY (`nb_id`);
-
---
 -- Indexes for table `tbl_pejabat`
 --
 ALTER TABLE `tbl_pejabat`
@@ -587,16 +564,18 @@ ALTER TABLE `tbl_tahun`
 --
 ALTER TABLE `tbl_transaksi`
   ADD PRIMARY KEY (`trx_id`),
-  ADD KEY `trx_id_nomor_bukti` (`trx_id_nomor_bukti`),
   ADD KEY `trx_id_jenis_pembayaran` (`trx_id_jenis_pembayaran`),
   ADD KEY `trx_id_metode_pembayaran` (`trx_id_metode_pembayaran`),
-  ADD KEY `trx_id_unit` (`trx_id_unit`);
+  ADD KEY `trx_id_tahun` (`trx_id_tahun`);
 
 --
--- Indexes for table `unit`
+-- Indexes for table `tbl_transaksi_unit`
 --
-ALTER TABLE `unit`
-  ADD PRIMARY KEY (`id_unit`);
+ALTER TABLE `tbl_transaksi_unit`
+  ADD PRIMARY KEY (`trxu_id`),
+  ADD KEY `trx_id_metode_pembayaran` (`trxu_id_metode_pembayaran`),
+  ADD KEY `trxu_id_jenis_pembayaran` (`trxu_id_jenis_pembayaran`),
+  ADD KEY `trxu_nomor_bukti` (`trxu_nomor_bukti`);
 
 --
 -- Indexes for table `users`
@@ -627,13 +606,13 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT for table `menu_type`
@@ -651,19 +630,13 @@ ALTER TABLE `tbl_bulan`
 -- AUTO_INCREMENT for table `tbl_jenis_pembayaran`
 --
 ALTER TABLE `tbl_jenis_pembayaran`
-  MODIFY `jp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `jp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_metode_pembayaran`
 --
 ALTER TABLE `tbl_metode_pembayaran`
   MODIFY `mp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tbl_nomor_bukti`
---
-ALTER TABLE `tbl_nomor_bukti`
-  MODIFY `nb_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_pejabat`
@@ -699,13 +672,13 @@ ALTER TABLE `tbl_tahun`
 -- AUTO_INCREMENT for table `tbl_transaksi`
 --
 ALTER TABLE `tbl_transaksi`
-  MODIFY `trx_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `trx_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `unit`
+-- AUTO_INCREMENT for table `tbl_transaksi_unit`
 --
-ALTER TABLE `unit`
-  MODIFY `id_unit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+ALTER TABLE `tbl_transaksi_unit`
+  MODIFY `trxu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -727,10 +700,17 @@ ALTER TABLE `users_groups`
 -- Constraints for table `tbl_transaksi`
 --
 ALTER TABLE `tbl_transaksi`
-  ADD CONSTRAINT `tbl_transaksi_ibfk_1` FOREIGN KEY (`trx_id_nomor_bukti`) REFERENCES `tbl_nomor_bukti` (`nb_id`),
   ADD CONSTRAINT `tbl_transaksi_ibfk_2` FOREIGN KEY (`trx_id_jenis_pembayaran`) REFERENCES `tbl_jenis_pembayaran` (`jp_id`),
   ADD CONSTRAINT `tbl_transaksi_ibfk_3` FOREIGN KEY (`trx_id_metode_pembayaran`) REFERENCES `tbl_metode_pembayaran` (`mp_id`),
-  ADD CONSTRAINT `tbl_transaksi_ibfk_4` FOREIGN KEY (`trx_id_unit`) REFERENCES `unit` (`id_unit`);
+  ADD CONSTRAINT `tbl_transaksi_ibfk_4` FOREIGN KEY (`trx_id_tahun`) REFERENCES `tbl_tahun` (`tahun_id`);
+
+--
+-- Constraints for table `tbl_transaksi_unit`
+--
+ALTER TABLE `tbl_transaksi_unit`
+  ADD CONSTRAINT `tbl_transaksi_unit_ibfk_1` FOREIGN KEY (`trxu_id_jenis_pembayaran`) REFERENCES `tbl_jenis_pembayaran` (`jp_id`),
+  ADD CONSTRAINT `tbl_transaksi_unit_ibfk_2` FOREIGN KEY (`trxu_id_metode_pembayaran`) REFERENCES `tbl_metode_pembayaran` (`mp_id`),
+  ADD CONSTRAINT `tbl_transaksi_unit_ibfk_3` FOREIGN KEY (`trxu_nomor_bukti`) REFERENCES `tbl_transaksi` (`trx_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
